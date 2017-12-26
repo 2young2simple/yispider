57d48bdb0f8ed9cbd05e754488b5b27d326b49b0
yixiangguo <793123055@qq.com>
2017-12-26 17:21:23 +0800

    fix more stmt [mysql]

--- spider/pipline/mysql/mysql.go
+++ spider/pipline/mysql/mysql.go
@@ -4,33 +4,26 @@ import "database/sql"
 import (
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/astaxie/beego"
-	"sync"
+	"github.com/astaxie/beego/orm"
 )
 
 var DB *sql.DB
-var once sync.Once
 
-func InitMysql(msql string) {
-	once.Do(func(){
-		var err error
-		DB, err = sql.Open("mysql", msql)
-		if err != nil {
-			beego.Error("连接数据库 【失败】")
-			panic(err)
-		}
-		beego.Info("连接数据库 【完成】")
-	})
+func InitMysql(mysql string) {
+	orm.RegisterDriver("mysql", orm.DRMySQL)
+	orm.RegisterDataBase("default", "mysql", mysql)
+	orm.RegisterModel(&C{})
+	orm.RunSyncdb("default", false, true)
+}
+
+type C struct {
+	Id int
 }
 
 func CreateTable(m *DBModel) error{
-	stmt, err := DB.Prepare(m.TableSql())
-	if err != nil {
-		beego.Error(err)
-		return err
-	}
-	_, err = stmt.Exec()
-	if err != nil {
-		beego.Error(err)
+	o := orm.NewOrm()
+	_,err := o.Raw(m.TableSql()).Exec()
+	if err != nil{
 		return err
 	}
 	beego.Info("创建表 ",m.Name," 成功 【完成】")
@@ -38,17 +31,9 @@ func CreateTable(m *DBModel) error{
 }
 
 func Add(m *DBModel) error{
-
-	stmt, err := DB.Prepare(m.InsertSql())
-	if err != nil {
-		beego.Error("44 ",err)
-		return err
-	}
-	defer stmt.Close()
-
-	_, err = stmt.Exec(m.InsertArgs()...)
-	if err != nil {
-		beego.Error(err)
+	o := orm.NewOrm()
+	_,err := o.Raw(m.InsertSql(),m.InsertArgs()...).Exec()
+	if err != nil{
 		return err
 	}
 	beego.Info("插入数据成功")
